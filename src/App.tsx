2196003964d5c7fb816d2482f6cf841134619044
Johnny Black <john.p.black89@gmail.com>
2025-02-04 21:22:20 -0500

    Switch to React

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import reactLogo from "./assets/images/react_logo.svg";
+import cppLogo from "./assets/images/cpp_logo.png";
+import gitLogo from "./assets/images/Git_logo.svg";
+
+import "./App.css";
+
+function App() {
+	return (
+		<>
+			<div>
+				<img src={cppLogo} className="logo" alt="C++ logo" />
+				<img src={reactLogo} className="logo" alt="React logo" />
+				<img src={gitLogo} className="logo" alt="Git logo" />
+			</div>
+		</>
+	);
+}
+
+export default App;
