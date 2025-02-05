24d5306232f9a387f4cb3c8e9690af1c5005403a
Johnny Black <john.p.black89@gmail.com>
2025-02-05 11:20:10 -0500

    Added Motion

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import "./ss/app.css";
 import TechKnown from "./components/TechKnown";
 import Name from "./components/Name";
-import MotionTest from "./components/MotionTest";
 
 function App() {
 	return (
