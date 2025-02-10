34dec2c040268b27a32c3e43a3f4d6c99a41c40d
Johnny Black <john.p.black89@gmail.com>
2025-02-10 00:03:05 -0500

    sass import path update

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "../dist/css/index.min.css";
 import App from "./App.tsx";
+import "../dist/assets/index-CDkGHseO.css";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
